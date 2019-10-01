23d44ca4682e29527066614fc994f3bd0fe5ed7b
Albert <albert@jensen.com.au>
2019-10-01 10:06:28 +1000

    Finish the search function

--- employees/src/components/App.js
+++ employees/src/components/App.js
@@ -7,29 +7,49 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.toggleShow = this.toggleShow.bind(this);
-    this.employee={};
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.updateselectedValue =this.updateselectedValue.bind(this);
+    this.employee = {};
 
     this.state = {
       error: null,
       isLoaded: false,
       data: {},
       isShow: false,
-      highLight : ''
+      highLight: '',
+      inputValue:'',
+      selectedValue:'first name'
     };
   }
 
   // showDetails will show the pop-up windows to show the employee deatils.
-  toggleShow = (i) => {    
-    console.log("i is: ",i)    
-    if(Number.isInteger(i)){
-      this.employee =this.state.data.employees[i];
-      this.setState({highLight: this.state.data.employees[i].id});
-    }else{
-      this.employee =this.state.data.employees[0];
-    }    
-    this.setState(state => ({isShow: !state.isShow }));
+  toggleShow = (i) => {
+    console.log("i is: ", i)
+    if (Number.isInteger(i)) {
+      this.employee = this.state.data.employees[i];
+      this.setState({ highLight: this.state.data.employees[i].id });
+    } else {
+      this.employee = this.state.data.employees[0];
+    }
+    this.setState(state => ({ isShow: !state.isShow }));
   }
 
+  // update the InputValue state
+  updateInputValue=(evt) =>{
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+    // update the selectedValue state
+    updateselectedValue=(evt) =>{
+      this.setState({
+        selectedValue: evt.target.value
+      });
+      console.log("selected value is: ",this.state.selectedValue);
+    }
+
+    
   // Using componentDidMount to fetch the sample-data.json file.
   componentDidMount() {
     fetch('./data/sample-data.json', {
@@ -66,37 +86,45 @@ class App extends Component {
       return <div>Loading...</div>;
     } else {
 
-      // Generate the employee Info to each Column, then use in the Row.
-      const employeeInfo = [];
-      let className =null;
-      for (let i = 0; i < data.employees.length; i++) {
-        data.employees[i].dateJoined=data.employees[i].dateJoined.slice(0,10);
-        if(data.employees[i].id==this.state.highLight && this.state.isShow){
-           className= 'card card-highlight'
-        }else{
-          className=null 
+      // Generate the employee Info to each Column, then use in the Row.      
+      let className = null;
+
+      let employeeInfoFilter = [];
+      this.state.data.employees.map((employee,i) => {
+        employee.dateJoined = employee.dateJoined.slice(0, 10)
+        if (employee.id == this.state.highLight && this.state.isShow) {
+          className = 'card card-highlight'
+        } else {
+          className = null
         }
-        employeeInfo.push(
-          <Col            
-            xs={12} sm={6} md={4}
-            onClick={() => this.toggleShow(i)}
-          >
-            <div key={data.employees[i].id} className={className}  style={{ marginBottom: 1 + 'rem' }}>
-              <div class="card-body clearfix" style={{ padding: 0 }}>
-                <div class="row">
-                  <div class="col-6 text-left">
-                    <img class="img2" src={data.employees[i].avatar} alt={data.employees[i].firstName + " " + data.employees[i].lastName} />
-                  </div>
-                  <div class="col-6">
-                    <h5 class="card-title">{data.employees[i].firstName} {data.employees[i].lastName}</h5>
-                    <p class="card-text">{data.employees[i].bio.slice(0, 30)}</p>
+
+        if ((this.state.selectedValue==="firstName" && employee.firstName.includes(this.state.inputValue)) || 
+            (this.state.selectedValue==="lastName" &&  employee.lastName.includes(this.state.inputValue))) {
+          employeeInfoFilter.push(
+            <Col
+              xs={12} sm={6} md={4}
+              onClick={() => this.toggleShow(i)}
+            >
+              <div key={employee.id} className={className} style={{ marginBottom: 1 + 'rem' }}>
+                <div class="card-body clearfix" style={{ padding: 0 }}>
+                  <div class="row">
+                    <div class="col-6 text-left">
+                      <img class="img2" src={employee.avatar} alt={employee.firstName + " " + employee.lastName} />
+                    </div>
+                    <div class="col-6">
+                      <h5 class="card-title">{employee.firstName} {employee.lastName}</h5>
+                      <p class="card-text">{employee.bio.slice(0, 30)}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Col>
-        )
-      }
+            </Col>
+          )
+        }
+
+      });
+
+
       return (
         <Container>
 
@@ -130,21 +158,22 @@ class App extends Component {
 
               <Col xs={6} sm={6} md={4}>
                 Sort by:&nbsp;
-                  <select name="sort" placeholder="first name">
+                  <select name="sort" placeholder="first name" onChange={this.updateselectedValue}>
                   <option value="firstName">first name</option>
                   <option value="lastName">last name</option>
                 </select>
               </Col>
 
               <Col xs={12} sm={12} md={4}>
-                Serach&nbsp; <input></input>
+                Serach&nbsp; <input id="search-name" onChange={this.updateInputValue}></input>
               </Col>
             </Row>
 
             {/* Showing all the employees info in the Row with employeeInfo columns */}
             <Row>
-              {employeeInfo}
+              {employeeInfoFilter}
             </Row>
+
           </div>
         </Container >
       );
